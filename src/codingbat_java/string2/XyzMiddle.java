caebae07920b7e2c3f036f29be993beb6da7fa2d
Utpal <utpal.kumar74@gmail.com>
2020-04-26 17:40:44 +0530

    Adding comment

--- src/codingbat_java/string2/XyzMiddle.java
+++ src/codingbat_java/string2/XyzMiddle.java
@@ -1,5 +1,17 @@
 package codingbat_java.string2;
 
+/**
+ * Given a string, does "xyz" appear in the middle of the string? To define
+ * middle, we'll say that the number of chars to the left and right of the "xyz"
+ * must differ by at most one. This problem is harder than it looks.
+ * 
+ * 
+ * xyzMiddle("AAxyzBB") → true xyzMiddle("AxyzBB") → true xyzMiddle("AxyzBBB") →
+ * false
+ * 
+ * @author utpal
+ *
+ */
 public class XyzMiddle {
 
 	public boolean xyzMiddle(String str) {
